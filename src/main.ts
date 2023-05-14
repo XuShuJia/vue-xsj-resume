db4ff286964503821930a5b17fc41d6911ca6caa
XuShuJia <13428342592@163.com>
2023-05-14 16:17:35 +0800

    update

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import "normalize.css";
-import "@/styles/reset.less";
-import { createApp } from "vue";
-import Resume from "@/pages/resume";
-
-createApp(Resume).mount(document.getElementById("app") as HTMLDivElement);
